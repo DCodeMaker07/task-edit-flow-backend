ac61bf8595e4ffb822f40bfedbe884edd23abddd
DelioCoder <davideveriwhere@gmail.com>
2026-04-28 18:28:09 -0500

    nestJS/Swagger setted

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
 import { ResponseInterceptor } from './common/interceptors/response.interceptor';
@@ -22,8 +23,19 @@ async function bootstrap() {
 
   app.useGlobalInterceptors(new ResponseInterceptor()); // Global response interceptor
 
+  // Swagger configuración
+  const config = new DocumentBuilder()
+    .setTitle('TaskFlow Pro API')
+    .setDescription('Task Management System API')
+    .setVersion('1.0.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(port);
   console.log(`Application running on http://localhost:${port}`);
-  
+  console.log(`Swagger documentation at http://localhost:${port}/api/docs`);
 }
 bootstrap();
