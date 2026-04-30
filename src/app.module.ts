135178978abbcc8e766832418db5c6c3c30c510e
DelioCoder <davideveriwhere@gmail.com>
2026-04-29 19:36:02 -0500

    Added: rate-limit in imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,24 @@ import { ProjectsModule } from './modules/projects/projects.module';
 import { TasksModule } from './modules/tasks/tasks.module';
 import { PrismaService } from './prisma.service';
 import { SeedModule } from './modules/seed/seed.module';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
-  imports: [AuthModule, UsersModule, ProjectsModule, TasksModule, SeedModule],
+  imports: [
+    AuthModule,
+    UsersModule,
+    ProjectsModule,
+    TasksModule,
+    SeedModule,
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: 60000,
+          limit: 10,
+        },
+      ],
+    }),
+  ],
   providers: [PrismaService],
 })
-export class AppModule {}
+export class AppModule { }
